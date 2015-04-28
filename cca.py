c9b0c6c7202bd52ec843d450eb9123ee32b380ce
rupy <rupyapps@gmail.com>
2015-04-29 03:03:37 +0900

    made cca inherit from gcca

--- cca.py
+++ cca.py
@@ -3,131 +3,64 @@
 
 __author__ = 'rupy'
 
+from gcca import GCCA
 import numpy as np
-from scipy.linalg import eig
-import time
 import logging
 import os
-import sys
 import matplotlib.pyplot as plt
+import math
+from matplotlib import colors
+import h5py
 
-class CCA(object):
+class CCA(GCCA):
 
     def __init__(self, n_components=2, reg_param=0.1):
+        GCCA.__init__(self, n_components, reg_param)
 
         # log setting
-        program = os.path.basename(sys.argv[0])
+        program = os.path.basename(__name__)
         self.logger = logging.getLogger(program)
-        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+        logging.basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s')
 
-        # CCA params
-        self.n_components = n_components
-        self.reg_param = reg_param
+        self.z_p = np.array([])
 
-        # data
-        self.x = None
-        self.y = None
+    def fit(self, x0, x1):
 
-        # Result of fitting
-        self.x_weights = None
-        self.y_weights = None
-        self.eigvals = None
-        self.c_xx = None
-        self.c_yy = None
-        self.c_xy = None
+        x_list = [x0, x1]
 
-        # transformed data by CCA
-        self.x_c = None
-        self.y_c = None
-        self.z_c = None
+        # data size check
+        data_num = len(x_list)
+        self.logger.info("data num is %d", data_num)
+        for i, x in enumerate(x_list):
+            self.logger.info("data shape x_%d: %s", i, x.shape)
 
-        self.y_s = None
+        self.logger.info("normalizing")
+        x_norm_list = [ self.normalize(x) for x in x_list]
 
-    def solve_eigprob(self, left, right):
-
-        self.logger.info("calculating eigen dimension")
-        eig_dim = min([np.linalg.matrix_rank(left), np.linalg.matrix_rank(right)])
-
-        self.logger.info("calculating eigenvalues and eigenvector")
-        eig_vals, eig_vecs = eig(left, right)# ;print eig_vals.imag
-
-        self.logger.info("sorting eigenvalues and eigenvector")
-        sort_indices = np.argsort(eig_vals)[::-1]
-        eig_vals = eig_vals[sort_indices][:eig_dim].real
-        eig_vecs = eig_vecs[:,sort_indices][:,:eig_dim].real
-
-        # regularization
-        self.logger.info("regularizing")
-        # eig_vecs = np.dot(eig_vecs, np.diag(np.reciprocal(np.linalg.norm(eig_vecs, axis=0))))
-        var = np.dot(eig_vecs.T, np.dot(right, eig_vecs))
-        # print var
-        invvar = np.diag(np.reciprocal(np.sqrt(np.diag(var))))
-        # print invvar
-        eig_vecs = np.dot(eig_vecs, invvar)
-
-        print np.dot(eig_vecs.T, np.dot(right, eig_vecs)).round().astype(int)
-
-        return eig_vals, eig_vecs
-
-
-    def fit(self, x, y):
-
-        self.x = x
-        self.y = y
-
-        self.logger.info("calculating average, variance, and covariance")
-        z = np.vstack((x.T, y.T))
-        cov = np.cov(z)
-        p = len(x.T)
-        c_xx = cov[:p, :p]
-        c_yy = cov[p:, p:]
-        c_xy = cov[:p, p:]
-        # print c_xx.shape, c_xy.shape, c_yy.shape
+        d_list = [0] + [sum([len(x.T) for x in x_list][:i + 1]) for i in xrange(data_num)]
+        cov_mat = self.calc_cov_mat(x_norm_list)
+        c_00 = cov_mat[0][0]
+        c_01 = cov_mat[0][1]
+        c_11 = cov_mat[1][1]
 
         self.logger.info("calculating generalized eigenvalue problem ( A*u = (lambda)*B*u )")
-        self.logger.info("adding regularization term")
-        c_xx += self.reg_param * np.average(np.diag(c_xx)) * np.eye(c_xx.shape[0])
-        c_yy += self.reg_param * np.average(np.diag(c_yy)) * np.eye(c_yy.shape[0])
 
-        self.c_xx = c_xx
-        self.c_yy = c_yy
-        self.c_xy = c_xy
-
-        # left = A, right = B
-        self.logger.info("solving")
-        xleft = np.dot(c_xy, np.linalg.solve(c_yy,c_xy.T))
-        xright = c_xx
-        x_eigvals, x_eigvecs = self.solve_eigprob(xleft, xright)
-
-        yleft = np.dot(c_xy.T, np.linalg.solve(c_xx,c_xy))
-        yright = c_yy
-        y_eigvals, y_eigvecs = self.solve_eigprob(yleft, yright)
-
-        # y_eigvecs = (1 / np.sqrt(x_eigvals)) * c_yy * c_xy * x_eigvals
+        # 1
+        left_1 = np.dot(c_01, np.linalg.solve(c_11,c_01.T))
+        right_1 = c_00
+        eigvals_1, eigvecs_1 = self.solve_eigprob(left_1, right_1)
+        # 2
+        eigvecs_2 = 1 / eigvals_1 * np.dot(np.linalg.solve(c_11, c_01.T), eigvecs_1)
 
         # substitute local variables for member variables
-        self.x_weights = x_eigvecs
-        self.eigvals = x_eigvals
-        self.y_weights = y_eigvecs
+        self.data_num = data_num
+        self.cov_mat = cov_mat
+        self.h_list = [eigvecs_1, eigvecs_2]
+        self.eigvals = eigvals_1
 
-    def transform(self, x, y):
+    def ptransform(self, x0, x1, beta=0.5):
 
-        self.logger.info("Normalizing")
-        x = self.normalize(x)
-        y = self.normalize(y)
-
-        self.logger.info("transform matrices by CCA")
-        x_projected = np.dot(x, self.x_weights)
-        y_projected = np.dot(y, self.y_weights)
-
-        self.x_c = x_projected
-        self.y_c = y_projected
-
-        return x_projected, y_projected
-
-    def ptransform(self, x, y, beta=0.5):
-
-        x_projected, y_projected = self.transform(x, y)
+        x0_projected, x1_projected = self.transform(x0, x1)
 
         I = np.eye(len(self.eigvals))
         lamb = np.diag(self.eigvals)
@@ -136,134 +69,73 @@ class CCA(object):
         mat12 = np.vstack((mat1, mat2))
         mat21 = np.vstack((mat2, mat1))
         mat = np.hstack((mat12, mat21))
-        # print lamb.shape, lamb
         p = np.vstack((lamb**beta, lamb**(1-beta)))
-        q = np.vstack((x_projected.T, y_projected.T))
-        # print p.T.shape, mat.shape, q.shape
+        q = np.vstack((x0_projected.T, x1_projected.T))
         z = np.dot(p.T, np.dot(mat, q)).T[:,:self.n_components]
 
-        self.x_c = x_projected
-        self.y_c = y_projected
-        self.z_c = z
+        self.z_p = z
 
-        return x_projected, y_projected, z
-
-    def fit_transform(self, x, y):
-        self.fit(x, y)
-        return self.transform(x, y)
-
-    def fit_ptransform(self, x, y, beta=0.5):
-        self.fit(x, y)
-        return self.ptransform(x, y, beta)
+        return x0_projected, x1_projected, z
 
     def save_params(self, filepath):
-        self.logger.info("saving cca")
-        np.save(filepath + "n_components.npy" , self.n_components)
-        np.save(filepath + "reg_param.npy", self.reg_param)
-        np.save(filepath + "x_weights.npy", self.x_weights)
-        np.save(filepath + "y_weights.npy", self.y_weights)
-        np.save(filepath + "eigvals.npy", self.eigvals)
-        np.save(filepath + "cxx.npy", self.c_xx)
-        np.save(filepath + "cyy.npy", self.c_yy)
-        np.save(filepath + "cxy.npy", self.c_xy)
+
+        GCCA.save_params(self, filepath)
+        if len(self.z_p) != 0:
+            with h5py.File(filepath, 'a') as f:
+                f.create_dataset("z_p", data=self.z_p)
+                f.flush()
 
     def load_params(self, filepath):
-        self.logger.info("loading cca")
-        self.n_components = np.load(filepath + "n_components.npy")
-        self.reg_param = np.load(filepath + "reg_param.npy")
-        self.x_weights = np.load(filepath + "x_weights.npy")
-        self.y_weights = np.load(filepath + "y_weights.npy")
-        self.eigvals = np.load(filepath + "eigvals.npy")
-        self.c_xx = np.load(filepath + "cxx.npy")
-        self.c_yy = np.load(filepath + "cyy.npy")
-        self.c_xy = np.load(filepath + "cxy.npy")
 
-    def check_fit_finished(self):
-        return self.x_weights is not None\
-               and self.y_weights is not None\
-               and self.eigvals is not None\
-               and self.x is not None\
-               and self.y is not None\
-               and self.c_xx is not None\
-               and self.c_yy is not None\
-               and self.c_xy is not None
+        GCCA.load_params(self, filepath)
 
-    def plot_original_data(self, X, Y):
-        plt.subplot(221)
-        plt.plot(X[:, 0], X[:, 1], 'xb')
-        plt.title('X')
+        with h5py.File(filepath, "r") as f:
+            if "z_p" in f:
+                self.z_p = f["z_p"].value
+            f.flush()
+    def plot_cca_result(self):
+        self.plot_gcca_result()
 
-        plt.subplot(222)
-        plt.plot(Y[:, 0], Y[:, 1], '.r')
-        plt.title('Y')
+    def plot_pcca_result(self):
 
-        plt.show()
-
-
-    def plot_cca_result(self, probabilistic=True):
-
-        X = None
-        Y = None
-        Z = None
-        if probabilistic:
-            self.logger.info("plotting PCCA")
-            X = self.x_c
-            Y = self.y_c
-            Z = self.z_c
-        else:
-            self.logger.info("plotting CCA")
-            X = self.x_c
-            Y = self.y_c
-
-        # correct direction
-        cor_signs = np.sign([np.corrcoef(X[:, i], Y[:, i])[0, 1] for i in xrange(X.shape[1])])
-        Y_s = Y * cor_signs
+        self.logger.info("plotting gcca result")
+        row_num = 2
+        col_num = 2
 
         # begin plot
         plt.figure()
 
-        plt.subplot(221)
-        plt.plot(X[:, 0], X[:, 1], 'xb')
-        plt.plot(Y_s[:, 0], Y_s[:, 1], '.r')
-        plt.title('CCA XY')
+        color_list = colors.cnames.keys()
+        plt.subplot(row_num, col_num, 1)
+        plt.plot(self.z_list[0][:, 0], self.z_list[0][:, 1], c=color_list[0], marker='.', ls=' ')
+        plt.title('Z_0(CCA)')
+        plt.subplot(row_num, col_num, 2)
+        plt.plot(self.z_list[1][:, 0], self.z_list[1][:, 1], c=color_list[1], marker='.', ls=' ')
+        plt.title('Z_1(CCA)')
 
-        plt.subplot(222)
-        plt.plot(X[:, 0], X[:, 1], 'xb')
-        plt.title('CCA X')
+        plt.subplot(row_num, col_num, 3)
+        plt.plot(self.z_list[0][:, 0], self.z_list[0][:, 1], c=color_list[0], marker='.', ls=' ')
+        plt.plot(self.z_list[1][:, 0], self.z_list[1][:, 1], c=color_list[1], marker='.', ls=' ')
+        plt.title('Z_ALL(CCA)')
 
-        plt.subplot(223)
-        plt.plot(Y_s[:, 0], Y_s[:, 1], '.r')
-        plt.title('CCA Y')
-
-        if probabilistic:
-            plt.subplot(224)
-            plt.plot(Z[:, 0], Z[:, 1], 'xb')
-            plt.title('CCA Z')
+        plt.subplot(row_num, col_num, 4)
+        plt.plot(self.z_p[:, 0], self.z_p[:, 1], c=color_list[2], marker='.', ls=' ')
+        plt.title('Z(PCCA)')
 
         plt.show()
 
-
-
-    def normalize(self, mat):
-        m = np.mean(mat, axis=0)
-        mat = mat - m
-        return mat
-
-    def corrcoef(self):
-        return np.corrcoef(self.x_c[:,0], self.y_c[:,0])
-
-    def fix_reverse(self):
-        cor = [np.corrcoef(self.x_c[:, i], self.y_c[:, i])[0, 1] for i in xrange(self.x_c.shape[1])]
-        cor_signs = np.sign(cor)
-        self.y_s = self.y_c * cor_signs
-
 if __name__=="__main__":
 
-    # Reduce dimensions of x, y from 30, 20 to 10 respectively.
-    x = np.random.random((100, 30))
-    y = np.random.random((100, 20))
-    cca = CCA(n_components=10, reg_param=0.1)
-    x_c, y_c = cca.fit_transform(x, y)
 
-    #
-    print np.corrcoef(x_c[:,0], y_c[:,0])
+    logging.root.setLevel(level=logging.INFO)
+
+    a = np.random.rand(50, 50)
+    b = np.random.rand(50, 60)
+
+    cca = CCA()
+    cca.fit(a, b)
+    cca.transform(a, b)
+    cca.ptransform(a, b)
+    cca.save_params("save/cca.h5")
+    cca.load_params("save/cca.h5")
+    cca.plot_pcca_result()
--- gcca.py
+++ gcca.py
@@ -7,207 +7,238 @@ import numpy as np
 from scipy.linalg import eig
 import logging
 import os
-import sys
 import matplotlib.pyplot as plt
+import math
+from matplotlib import colors
+import h5py
 
 class GCCA:
 
     def __init__(self, n_components=2, reg_param=0.1):
 
         # log setting
-        program = os.path.basename(sys.argv[0])
+        program = os.path.basename(__name__)
         self.logger = logging.getLogger(program)
-        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+        logging.basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s')
 
         # GCCA params
         self.n_components = n_components
         self.reg_param = reg_param
 
-        # data
-        self.x_1 = None
-        self.x_2 = None
-        self.x_3 = None
+        # result of fitting
+        self.data_num = 0
+        self.cov_mat = [[]]
+        self.h_list = []
+        self.eigvals = np.array([])
 
-        # Result of fitting
-        self.c11 = None
-        self.c22 = None
-        self.c33 = None
-        self.c12 = None
-        self.c23 = None
-        self.c13 = None
-        self.h_1 = None
-        self.h_2 = None
-        self.h_3 = None
-        self.eigvals = None
+        # result of transformation
+        self.z_list = []
 
     def solve_eigprob(self, left, right):
 
         self.logger.info("calculating eigen dimension")
         eig_dim = min([np.linalg.matrix_rank(left), np.linalg.matrix_rank(right)])
 
-        self.logger.info("calculating eigenvalues and eigenvector")
-        eig_vals, eig_vecs = eig(left, right)# ;print eig_vals.imag
+        self.logger.info("calculating eigenvalues & eigenvector")
+        eig_vals, eig_vecs = eig(left, right)
 
-        self.logger.info("sorting eigenvalues and eigenvector")
+        self.logger.info("sorting eigenvalues & eigenvector")
         sort_indices = np.argsort(eig_vals)[::-1]
         eig_vals = eig_vals[sort_indices][:eig_dim].real
         eig_vecs = eig_vecs[:,sort_indices][:,:eig_dim].real
 
-        # regularization
-        self.logger.info("regularizing")
-        # eig_vecs = np.dot(eig_vecs, np.diag(np.reciprocal(np.linalg.norm(eig_vecs, axis=0))))
+        # normalization
+        self.logger.info("normalization")
         var = np.dot(eig_vecs.T, np.dot(right, eig_vecs))
-        # print var
         invvar = np.diag(np.reciprocal(np.sqrt(np.diag(var))))
-        # print invvar
         eig_vecs = np.dot(eig_vecs, invvar)
-
-        print np.dot(eig_vecs.T, np.dot(right, eig_vecs)).round().astype(int)
 
         return eig_vals, eig_vecs
 
+    def calc_cov_mat(self, x_list):
 
-    def fit(self, x_1, x_2, x_3):
+        data_num = len(x_list)
 
-        self.x_1 = x_1
-        self.x_2 = x_2
-        self.x_3 = x_3
-
-        self.logger.info("calculating average, variance, and covariance")
-        z = np.vstack((x_1.T, x_2.T, x_3.T))
-        print z.shape
+        self.logger.info("calc variance & covariance matrix")
+        z = np.vstack([x.T for x in x_list])
         cov = np.cov(z)
-        d1 = len(x_1.T)
-        d2 = len(x_2.T) + d1
-        print d1, d2
-        c11 = cov[:d1, :d1]
-        c22 = cov[d1:d2, d1:d2]
-        c33 = cov[d2:, d2:]
-        c12 = cov[:d1, d1:d2]
-        c23 = cov[d1:d2, d2:]
-        c13 = cov[:d1, d2:]
-        # print c11.shape
-        # print c22.shape
-        # print c33.shape
-        # print c12.shape
-        # print c23.shape
-        # print c13.shape
+        d_list = [0] + [sum([len(x.T) for x in x_list][:i + 1]) for i in xrange(data_num)]
+        cov_mat = [[np.array([]) for col in range(data_num)] for row in range(data_num)]
+        for i in xrange(data_num):
+            for j in xrange(data_num):
+                i_start, i_end = d_list[i], d_list[i + 1]
+                j_start, j_end = d_list[j], d_list[j + 1]
+                cov_mat[i][j] = cov[i_start:i_end, j_start:j_end]
+
+        # regularization
+        self.logger.info("adding regularization term")
+        for i in xrange(data_num):
+            cov_mat[i][i] += self.reg_param * np.average(np.diag(cov_mat[i][i])) * np.eye(cov_mat[i][i].shape[0])
+
+        return cov_mat
+
+    def fit(self, *x_list):
+
+        # data size check
+        data_num = len(x_list)
+        self.logger.info("data num is %d", data_num)
+        for i, x in enumerate(x_list):
+            self.logger.info("data shape x_%d: %s", i, x.shape)
+
+        self.logger.info("normalizing")
+        x_norm_list = [ self.normalize(x) for x in x_list]
+
+        d_list = [0] + [sum([len(x.T) for x in x_list][:i + 1]) for i in xrange(data_num)]
+        cov_mat = self.calc_cov_mat(x_norm_list)
 
         self.logger.info("calculating generalized eigenvalue problem ( A*u = (lambda)*B*u )")
-        self.logger.info("adding regularization term")
-        c11 += self.reg_param * np.average(np.diag(c11)) * np.eye(c11.shape[0])
-        c22 += self.reg_param * np.average(np.diag(c22)) * np.eye(c22.shape[0])
-        c33 += self.reg_param * np.average(np.diag(c33)) * np.eye(c33.shape[0])
-
         # left = A, right = B
+        left = 0.5 * np.vstack(
+            [
+                np.hstack([np.zeros_like(cov_mat[i][j]) if i == j else cov_mat[i][j] for j in xrange(data_num)])
+                for i in xrange(data_num)
+            ]
+        )
+        right = np.vstack(
+            [
+                np.hstack([np.zeros_like(cov_mat[i][j]) if i != j else cov_mat[i][j] for j in xrange(data_num)])
+                for i in xrange(data_num)
+            ]
+        )
+
+        # calc GEV
         self.logger.info("solving")
-        left = 0.5 * np.vstack([
-            np.hstack([np.zeros_like(c11), c12, c13]),
-            np.hstack([c12.T, np.zeros_like(c22), c23]),
-            np.hstack([c13.T, c23.T, np.zeros_like(c33)])
-        ])
-        right = np.vstack([
-            np.hstack([c11, np.zeros_like(c12), np.zeros_like(c13)]),
-            np.hstack([np.zeros_like(c12.T), c22, np.zeros_like(c23)]),
-            np.hstack([np.zeros_like(c13.T), np.zeros_like(c23.T), c33])
-        ])
         eigvals, eigvecs = self.solve_eigprob(left, right)
 
         # substitute local variables for member variables
-        self.h_1 = eigvecs[:d1]
-        self.h_2 = eigvecs[d1:d2]
-        self.h_3 = eigvecs[d2:]
+        self.data_num = data_num
+        self.cov_mat = cov_mat
+        self.h_list = [eigvecs[start:end] for start, end in zip(d_list[0:-1], d_list[1:])]
         self.eigvals = eigvals
-        self.c11 = c11
-        self.c22 = c22
-        self.c33 = c33
-        self.c12 = c12
-        self.c23 = c23
-        self.c13 = c13
 
-    def transform(self, x_1, x_2, x_3):
+    def transform(self, *x_list):
 
-        self.logger.info("Normalizing")
-        x_1 = self.normalize(x_1)
-        x_2 = self.normalize(x_2)
-        x_3 = self.normalize(x_3)
+        # data size check
+        data_num = len(x_list)
+        self.logger.info("data num is %d", data_num)
+        for i, x in enumerate(x_list):
+            self.logger.info("data shape x_%d: %s", i, x.shape)
+
+        if self.data_num != data_num:
+            raise Exception('data num when fitting is different from data num to be transformed')
+
+        self.logger.info("normalizing")
+        x_norm_list = [ self.normalize(x) for x in x_list]
 
         self.logger.info("transform matrices by GCCA")
-        z_1 = np.dot(x_1, self.h_1)
-        z_2 = np.dot(x_2, self.h_2)
-        z_3 = np.dot(x_3, self.h_3)
+        z_list = [np.dot(x, h_vec) for x, h_vec in zip(x_norm_list, self.h_list)]
 
-        self.z_1 = z_1
-        self.z_2 = z_2
-        self.z_3 = z_3
+        self.z_list = z_list
 
-        return z_1, z_2, z_3
+        return z_list
 
-    def fit_transform(self, x_1, x_2, x_3):
-        self.fit(x_1, x_2, x_3)
-        self.transform(x_1, x_2, x_3)
+    def fit_transform(self, *x_list):
+        self.fit(x_list)
+        self.transform(x_list)
 
-    def save_params(self, filepath):
-        self.logger.info("saving gcca")
-        np.save(filepath + "n_components.npy" , self.n_components)
-        np.save(filepath + "reg_param.npy", self.reg_param)
-        np.save(filepath + "h_1.npy", self.h_1)
-        np.save(filepath + "h_2.npy", self.h_2)
-        np.save(filepath + "h_3.npy", self.h_3)
-        np.save(filepath + "eigvals.npy", self.eigvals)
-        np.save(filepath + "c11.npy", self.c11)
-        np.save(filepath + "c22.npy", self.c22)
-        np.save(filepath + "c33.npy", self.c33)
-        np.save(filepath + "c12.npy", self.c12)
-        np.save(filepath + "c23.npy", self.c23)
-        np.save(filepath + "c13.npy", self.c13)
-
-    def load_params(self, filepath):
-        self.logger.info("loading gcca")
-        self.n_components = np.load(filepath + "n_components.npy")
-        self.reg_param = np.load(filepath + "reg_param.npy")
-        self.h_1 = np.load(filepath + "h_1.npy")
-        self.h_2 = np.load(filepath + "h_2.npy")
-        self.h_3 = np.load(filepath + "h_3.npy")
-        self.eigvals = np.load(filepath + "eigvals.npy")
-        self.c11 = np.load(filepath + "c11.npy")
-        self.c22 = np.load(filepath + "c22.npy")
-        self.c33 = np.load(filepath + "c33.npy")
-        self.c12 = np.load(filepath + "c12.npy")
-        self.c23 = np.load(filepath + "c23.npy")
-        self.c13 = np.load(filepath + "c13.npy")
-
-    def normalize(self, mat):
+    @staticmethod
+    def normalize(mat):
         m = np.mean(mat, axis=0)
         mat = mat - m
         return mat
 
+    def save_params(self, filepath):
+
+        self.logger.info("saving gcca to %s", filepath)
+        with h5py.File(filepath, 'w') as f:
+            f.create_dataset("n_components", data=self.n_components)
+            f.create_dataset("reg_param", data=self.reg_param)
+            f.create_dataset("data_num", data=self.data_num)
+
+            cov_grp = f.create_group("cov_mat")
+            for i, row in enumerate(self.cov_mat):
+                for j, cov in enumerate(row):
+                    cov_grp.create_dataset(str(i) + "_" + str(j), data=cov)
+
+            h_grp = f.create_group("h_list")
+            for i, h in enumerate(self.h_list):
+                h_grp.create_dataset(str(i), data=h)
+
+            f.create_dataset("eig_vals", data=self.eigvals)
+
+            if len(self.z_list) != 0:
+                z_grp = f.create_group("z_list")
+                for i, z in enumerate(self.z_list):
+                    z_grp.create_dataset(str(i), data=z)
+            f.flush()
+
+    def load_params(self, filepath):
+        self.logger.info("loading gcca from %s", filepath)
+        with h5py.File(filepath, "r") as f:
+            self.n_components = f["n_components"].value
+            self.reg_param = f["reg_param"].value
+            self.data_num = f["data_num"].value
+
+            self.cov_mat = [[np.array([]) for col in range(self.data_num)] for row in range(self.data_num)]
+            for i in xrange(self.data_num):
+                for j in xrange(self.data_num):
+                    self.cov_mat[i][j] = f["cov_mat/" + str(i) + "_" + str(j)]
+            self.h_list = [None] * self.data_num
+            for i in xrange(self.data_num):
+                self.h_list[i] = f["h_list/" + str(i)].value
+            self.eig_vals = f["eig_vals"].value
+
+            if "z_list" in f:
+                self.z_list = [None] * self.data_num
+                for i in xrange(self.data_num):
+                    self.z_list[i] = f["z_list/" + str(i)].value
+            f.flush()
+
     def plot_gcca_result(self):
+
+        self.logger.info("plotting gcca result")
+        col_num = int(math.ceil(math.sqrt(self.data_num + 1)))
+        row_num = int((self.data_num + 1) / float(col_num))
+        if row_num != (self.data_num + 1) / float(col_num):
+            row_num += 1
 
         # begin plot
         plt.figure()
 
-        plt.subplot(221)
-        plt.plot(self.z_1[:, 0], self.z_1[:, 1], '.r')
-        plt.title('GCCA Z1(English)')
+        color_list = colors.cnames.keys()
+        for i in xrange(self.data_num):
 
-        plt.subplot(222)
-        plt.plot(self.z_2[:, 0], self.z_2[:, 1], '.g')
-        plt.title('GCCA Z2(Image)')
+            plt.subplot(row_num, col_num, i + 1)
+            plt.plot(self.z_list[i][:, 0], self.z_list[i][:, 1], c=color_list[i], marker='.', ls=' ')
+            plt.title("Z_%d(GCCA)" % (i + 1))
 
-        plt.subplot(223)
-        plt.plot(self.z_3[:, 0], self.z_1[:, 1], '.b')
-        plt.title('GCCA Z3(Japanese)')
-
-        plt.subplot(224)
-        plt.plot(self.z_1[:, 0], self.z_1[:, 1], '.r')
-        plt.plot(self.z_2[:, 0], self.z_2[:, 1], '.g')
-        plt.plot(self.z_3[:, 0], self.z_1[:, 1], '.b')
-        plt.title('GCCA Z123(ALL)')
+        plt.subplot(row_num, col_num, self.data_num + 1)
+        for i in xrange(self.data_num):
+            plt.plot(self.z_list[i][:, 0], self.z_list[i][:, 1], c=color_list[i], marker='.', ls=' ')
+            plt.title("Z_ALL(GCCA)")
 
         plt.show()
 
-
-
 if __name__=="__main__":
-    pass
+
+
+    logging.root.setLevel(level=logging.INFO)
+
+    a = np.random.rand(50, 50)
+    b = np.random.rand(50, 60)
+    c = np.random.rand(50, 70)
+    d = np.random.rand(50, 80)
+    e = np.random.rand(50, 90)
+    f = np.random.rand(50, 100)
+    g = np.random.rand(50, 110)
+    h = np.random.rand(50, 120)
+    i = np.random.rand(50, 130)
+    j = np.random.rand(50, 140)
+    k = np.random.rand(50, 150)
+
+    gcca = GCCA()
+    gcca.fit(a, b, c, d, e, f, g, h, i, j, k)
+    gcca.save_params("save/gcca.h5")
+    gcca.load_params("save/gcca.h5")
+    gcca.transform(a, b, c, d, e, f, g, h, i, j, k)
+    gcca.plot_gcca_result()

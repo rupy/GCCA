fixed normalization process bug

cca.py
@@ -50,13 +50,16 @@ class CCA(GCCA):
         left_1 = np.dot(c_01, np.linalg.solve(c_11,c_01.T))
         right_1 = c_00
         eigvals_1, eigvecs_1 = self.solve_eigprob(left_1, right_1)
+        eigvecs_1_norm = self.eigvec_normalization(eigvecs_1, right_1)
         # 2
-        eigvecs_2 = 1 / eigvals_1 * np.dot(np.linalg.solve(c_11, c_01.T), eigvecs_1)
+        right_2 = c_11
+        eigvecs_2 = 1 / eigvals_1 * np.dot(np.linalg.solve(c_11, c_01.T), eigvecs_1_norm)
+        eigvecs_2_norm = self.eigvec_normalization(eigvecs_2, right_2)
 
         # substitute local variables for member variables
         self.data_num = data_num
         self.cov_mat = cov_mat
-        self.h_list = [eigvecs_1, eigvecs_2]
+        self.h_list = [eigvecs_1_norm, eigvecs_2_norm]
         self.eigvals = eigvals_1
 
     def ptransform(self, x0, x1, beta=0.5):

gcca.py
@@ -34,6 +34,15 @@ class GCCA:
         # result of transformation
         self.z_list = []
 
+    def eigvec_normalization(self, eig_vecs, x_var):
+        self.logger.info("normalization")
+        z_var = np.dot(eig_vecs.T, np.dot(x_var, eig_vecs))
+        invvar = np.diag(np.reciprocal(np.sqrt(np.diag(z_var))))
+        eig_vecs = np.dot(eig_vecs, invvar)
+        # print np.dot(eig_vecs.T, np.dot(x_var, eig_vecs)).round().astype(int)
+        return eig_vecs
+
+
     def solve_eigprob(self, left, right):
 
         self.logger.info("calculating eigen dimension")
@@ -46,12 +55,6 @@ class GCCA:
         sort_indices = np.argsort(eig_vals)[::-1]
         eig_vals = eig_vals[sort_indices][:eig_dim].real
         eig_vecs = eig_vecs[:,sort_indices][:,:eig_dim].real
-
-        # normalization
-        self.logger.info("normalization")
-        var = np.dot(eig_vecs.T, np.dot(right, eig_vecs))
-        invvar = np.diag(np.reciprocal(np.sqrt(np.diag(var))))
-        eig_vecs = np.dot(eig_vecs, invvar)
 
         return eig_vals, eig_vecs
 
@@ -117,10 +120,13 @@ class GCCA:
         self.logger.info("solving")
         eigvals, eigvecs = self.solve_eigprob(left, right)
 
+        h_list = [eigvecs[start:end] for start, end in zip(d_list[0:-1], d_list[1:])]
+        h_list_norm = [self.eigvec_normalization(h, cov_mat[i][i]) for i, h in enumerate(h_list)]
+
         # substitute local variables for member variables
         self.data_num = data_num
         self.cov_mat = cov_mat
-        self.h_list = [eigvecs[start:end] for start, end in zip(d_list[0:-1], d_list[1:])]
+        self.h_list = h_list_norm
         self.eigvals = eigvals
 
     def transform(self, *x_list):